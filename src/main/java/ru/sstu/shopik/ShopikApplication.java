1840fd13e750cfb982465c4227b7c0897aee77bb
MaximKarabitsin <MaximKarabitsin@users.noreply.github.com>
2019-05-25 23:07:26 +0400

    create project

---
+++ src/main/java/ru/sstu/shopik/ShopikApplication.java
@@ -0,0 +1,13 @@
+package ru.sstu.shopik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopikApplication.class, args);
+    }
+
+}
