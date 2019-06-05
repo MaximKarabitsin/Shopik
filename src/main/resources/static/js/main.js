39f9d51166e3e93d6e5afcef44f623f187d7bd91
MaximKarabitsin <MaximKarabitsin@users.noreply.github.com>
2019-06-05 20:50:48 +0400

    add button lang

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
 
+    $(".user-menu-icon").on("click", function () {
+        $(".user-menu").toggleClass("user-menu--open");
+    });
 
-
-    $('.user-menu-icon').on('click', function () {
-        $('.user-menu').toggleClass('user-menu--open');
+    $(".lang-switch").on("click", function () {
+        var lang = $(this).attr("name");
+        $.cookie("lang", lang, { expires: 1 }); //1 day
+        location.reload();
     });
 
 });
