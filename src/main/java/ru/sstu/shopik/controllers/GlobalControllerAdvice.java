6a49cb83cbe517dbd71f7b4a23926aad3b5a8b42
MaximKarabitsin <MaximKarabitsin@users.noreply.github.com>
2019-06-09 14:47:18 +0400

    add th layouts

--- src/main/java/ru/sstu/shopik/controllers/GlobalControllerAdvice.java
+++ src/main/java/ru/sstu/shopik/controllers/GlobalControllerAdvice.java
@@ -1,18 +1,13 @@
 package ru.sstu.shopik.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import ru.sstu.shopik.dao.UserRepository;
-import ru.sstu.shopik.domain.UserDetailsImpl;
 import ru.sstu.shopik.domain.entities.User;
 import ru.sstu.shopik.services.UserService;
 
 import java.security.Principal;
-import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
