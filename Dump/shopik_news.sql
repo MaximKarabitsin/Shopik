8e818b653cd84207a6bfc378b801b431d7b8938a
MaximKarabitsin <MaximKarabitsin@users.noreply.github.com>
2019-06-17 18:30:37 +0400

    patch

--- Dump/shopik_news.sql
+++ Dump/shopik_news.sql
@@ -1,36 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
---
--- Host: localhost    Database: shopik
--- ------------------------------------------------------
--- Server version	8.0.15
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Dumping data for table `news`
---
-
-LOCK TABLES `news` WRITE;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-06-17  1:13:29
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (1, '2019-06-17', 'Открылся новый интернет магазин!', 'Мы открылись!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (2, '2019-06-17', 'Теперь вы можете выбрать количество покупаемого товара в корзине!', 'Обновление!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (3, '2019-06-17', 'Добавлена возможность занести понравившийся товар в список желаемого!', 'Новая возможность!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (4, '2019-06-17', 'Вы можете изменить настройки своего аккаунта во вкладке Профиль', 'Обновление!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (5, '2019-06-17', 'Вам доступна так же английская версия сайта', 'Знаете ли вы что..');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (6, '2019-06-17', 'В нашем магазине стала доступна покупка новой модели Samsung Galaxy A50!', 'Samsung Galaxy A50 уже в продаже!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (7, '2019-06-17', ' Успейте обзавестись летними товарами по отличным ценам!', 'Началась июньская распродажа!');
+INSERT INTO shopik.news (news_id, date, description, news_title) VALUES (8, '2019-06-17', 'В нашем магазине появился раздел мебели! Обставьте свой дом со вкусом с товарами из Shopik!', 'Новая возможность!');
